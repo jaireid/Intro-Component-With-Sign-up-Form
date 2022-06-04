c223bdc6148acb94c1046cdb00450c37111eb0b3
beareid <beareid.info@gmail.com>
2022-06-04 03:25:13 -0500

    Form Validation
    Error message and error icon display when form input fields are empty and when email is not properly formatted.

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 // Variables
 const form = document.getElementById("form");
-const firstName = document.getElementById("first-name")
-const firstNameErrorMessage = document.getElementById("first-name-error")
-
+const inputElements = document.querySelectorAll(".text-box");
 
 //Functions
-form.addEventListener('submit', (e) => {
-  if(firstName.value === "" || firstName.value === null) {
-    firstNameErrorMessage.className.add(".visiable");
-  }
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  inputElements.forEach((input) => {
+    const inputError = document.getElementById(`${input.id}-error`);
+    const iconError = document.getElementById(`${input.id}-icon`);
+    if (!input.value) {
+      inputError.classList.remove("hidden");
+      iconError.classList.remove("hidden");
+      input.classList.remove("grey-border");
+      input.placeholder = "";
+    } else {
+      inputError.classList.add("hidden");
+      iconError.classList.add("hidden");
+      input.classList.add("grey-border");
+    }
+    if (input.id === "email") {
+      let regex = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
+      let isCorrect = regex.test(input.value);
+      if (!isCorrect) {
+        inputError.classList.remove("hidden");
+        iconError.classList.remove("hidden");
+        input.classList.remove("grey-border");
+        input.placeholder = "email@example/com";
+      }
+      else {
+        inputError.classList.add("hidden");
+        iconError.classList.add("hidden");
+        input.classList.add("grey-border");
+      }
+    }
+  });
 });
